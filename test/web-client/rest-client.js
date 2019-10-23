a9155e7645076c0b2832479e9957924030651f4b
Miladinovic Bojan <fixed-term.bojan.miladinovic@se.bosch.com>
2019-10-23 11:32:46 +0200

    Update to VIS REST APi test page
    Improved look-and-feel of page
    Added log part of all executed requests and their responses
    
    For now simple REST requests can be sent (without HTTP request body
    data) to the VIS REST server.
    
    Signed-off-by: Miladinovic Bojan <fixed-term.bojan.miladinovic@se.bosch.com>

--- test/web-client/rest-client.js
+++ test/web-client/rest-client.js
@@ -18,33 +18,31 @@ document.getElementById('server-doc-root').value = "vss/api/v1";
 // setup event handlers
 document.getElementById('make-get-request').onclick = function req() {return makeRestRequest('GET');};
 document.getElementById('make-post-request').onclick = function req() {return makeRestRequest("POST");};
+document.getElementById('make-auth-request').onclick = function req() {return makeAuthRequest("POST");};
 
 
 document.getElementById('client-token').addEventListener('paste', onTokenUpdate);
-//document.getElementById('client-token').addEventListener('keyup', onTokenUpdate);
+document.getElementById('client-token').addEventListener('keyup', onTokenUpdate);
 document.getElementById('client-token').addEventListener('change', onTokenUpdate);
 
 document.getElementById('client-key-cert').addEventListener('paste', onTokenUpdate);
-//document.getElementById('client-key-cert').addEventListener('keyup', onTokenUpdate);
+document.getElementById('client-key-cert').addEventListener('keyup', onTokenUpdate);
 document.getElementById('client-key-cert').addEventListener('change', onTokenUpdate);
 
 ///////////////////
 // Implementation
 
 function onTokenUpdate() {
-  getEncodedToken();
+  if((document.getElementById("client-token").value != '') &&
+     (document.getElementById('client-key-cert').value != '')) {
+    getEncodedToken();
+  }
   return false;
 }
 
-function getBase64url(source) {
-  // make source base64 encoded
-  encodedSource = CryptoJS.enc.Base64.stringify(source);
-  // convert base64 base64url specifications
-  encodedSource = encodedSource.replace(/=+$/, '');
-  encodedSource = encodedSource.replace(/\+/g, '-');
-  encodedSource = encodedSource.replace(/\//g, '_');
-
-  return encodedSource;
+function onListResourceClick() {
+  document.getElementById('resource-path').value = this.innerText;
+  return false;
 }
 
 function getEncodedToken() {
@@ -69,6 +67,94 @@ function getEncodedToken() {
   return false;
 }
 
+function addReqToLog(type, resource) {
+  var newListItem = document.createElement("LI");
+  newListItem.setAttribute('class', 'li-request');
+
+  var newTypeDiv = document.createElement("div");
+  newTypeDiv.setAttribute('class', 'liType');
+  newTypeDiv.textContent = type;
+  newListItem.appendChild(newTypeDiv);
+
+  var newResourceDiv = document.createElement("div");
+  newResourceDiv.setAttribute('class', 'liResource');
+  newResourceDiv.textContent = resource;
+  newResourceDiv.onclick = onListResourceClick;
+  newListItem.appendChild(newResourceDiv);
+
+  var list = document.getElementById("log-list");
+  list.insertBefore(newListItem, list.childNodes[0]);
+
+}
+
+function addRespToLog(htmlStatus, message) {
+  var type = 'SUCCESS';
+
+  if (htmlStatus == 200) {
+  } else {
+    type = 'ERROR: HTML status = ' + htmlStatus;
+    // if no response
+    if (message == '') {
+      message = 'No response, check server status';
+    }
+  }
+  var newListItem = document.createElement("LI");
+  if (htmlStatus == 200) {
+    newListItem.setAttribute('class', 'li-response-ok');
+  } else {
+    newListItem.setAttribute('class', 'li-response-nok');
+  }
+
+  var newTypeDiv = document.createElement("div");
+  newTypeDiv.setAttribute('class', 'liType');
+  newTypeDiv.textContent = type;
+  newListItem.appendChild(newTypeDiv);
+
+  var newResourceDiv = document.createElement("div");
+  newResourceDiv.setAttribute('class', 'liResource');
+  newResourceDiv.textContent = message;
+  newListItem.appendChild(newResourceDiv);
+
+  var list = document.getElementById("log-list");
+  list.insertBefore(newListItem, list.childNodes[0]);
+
+}
+
+function makeHttpRequest(type, requestUrl, resourcePath) {
+  requestObj = new XMLHttpRequest();
+  var completeUrl = requestUrl + resourcePath;
+  requestObj.open(type, completeUrl, true);
+
+  addReqToLog(type, resourcePath);
+
+  // callback handler when positive response received
+  requestObj.onreadystatechange = function () {
+    if (this.readyState == XMLHttpRequest.DONE) {
+      addRespToLog(this.status, this.responseText);
+      if (this.status == 200) {
+        let target = '#json-table';
+        document.getElementById('json-table').innerHTML = '';
+        jsonView.format(this.responseText, target);
+      }
+    }
+  };
+  requestObj.send(null);
+}
+
+function makeAuthRequest(type) {
+  let server_address = document.getElementById('server-address').value;
+  let server_port    = document.getElementById('server-port').value;
+  let doc_root       = document.getElementById('server-doc-root').value;
+  let resource       = document.getElementById('auth-token-string').value;
+
+  if (server_address != "" && server_port != "") {
+    requestUrl = 'http://' + server_address + ':' + server_port + '/' + doc_root + '/';
+    makeHttpRequest(type, requestUrl, 'authorize?token=' + resource);
+  }
+  // prevent reloading of page
+  return false;
+}
+
 function makeRestRequest(type) {
   let server_address = document.getElementById('server-address').value;
   let server_port    = document.getElementById('server-port').value;
@@ -76,19 +162,8 @@ function makeRestRequest(type) {
   let resource       = document.getElementById('resource-path').value;
 
   if (server_address != "" && server_port != "") {
-    requestObj = new XMLHttpRequest();
-    requestUrl = 'http://' + server_address + ':' + server_port + '/' + doc_root + '/' + resource;
-    requestObj.open(type, requestUrl, true);
-
-    // callback handler when positive response received
-    requestObj.onreadystatechange = function () {
-      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        document.getElementById('json-raw-output').innerHTML = this.responseText;
-        let target = '#json-raw-output';
-        jsonView.format(this.responseText, target);
-      }
-    };
-    requestObj.send(null);
+    requestUrl = 'http://' + server_address + ':' + server_port + '/' + doc_root + '/';
+    makeHttpRequest(type, requestUrl, resource);
   }
   // prevent reloadig of page
   return false;
