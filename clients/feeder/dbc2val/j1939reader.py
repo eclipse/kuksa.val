f3a46d7e728dd5ce4d9bb99c8301f7ca78eeb811
Junhyung Ki <kijoonh91@gmail.com>
2020-12-07 15:26:23 +0100

    modified as @SebastianSchildt suggested in #81
    Change-Id: Ic1c317510c72f0af5aa8b015425361eae7e4ddae
    Signed-off-by: Junhyung Ki <kijoonh91@gmail.com>

--- clients/feeder/dbc2val/j1939reader.py
+++ clients/feeder/dbc2val/j1939reader.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python3
+
+########################################################################
+# Copyright (c) 2020 Robert Bosch GmbH
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+########################################################################
+
 """
 This script is to read CAN messages based on PGN - SAE J1939
 
