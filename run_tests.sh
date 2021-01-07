b039485f851cf0fa003de9e45edb6b35a6461c89
Chen Wenwen (CR/AME5) <Wenwen.Chen@de.bosch.com>
2021-01-07 19:18:48 +0100

    do not use lcov
    Signed-off-by: Chen Wenwen (CR/AME5) <Wenwen.Chen@de.bosch.com>

--- run_tests.sh
+++ run_tests.sh
@@ -35,17 +35,8 @@ cd ${builddir}
 cmake -DCMAKE_BUILD_TYPE=Coverage -DBUILD_UNIT_TEST=ON ..
 
 rm -rf coverage
-mkdir -p coverage
 
-lcov --zerocounters  --directory .
 ctest 
-lcov --directory . --capture --output-file kuksa.info.tmp
-lcov --extract kuksa.info.tmp "*${basepath}/include/*" --extract kuksa.info.tmp "*${basepath}/src/*" --output-file kuksa.info
-genhtml --output-directory ./coverage \
-  --demangle-cpp --num-spaces 2 --sort \
-  --title "My Program Test Coverage" \
-  --function-coverage --branch-coverage --legend \
-  kuksa.info
 gcovr -r ..  --branches -e ../test/ -e ../3rd-party-libs/ --xml -o coverage.xml
 
 if [ -d "${artifactdir}" ] 
@@ -54,5 +45,4 @@ echo "archive artifacts under ${artifactdir}"
 cd ${basepath}
 cp ${builddir}/test/unit-test/results.xml ${artifactdir}/
 cp ${builddir}/coverage.xml ${artifactdir}/
-cp -r ${builddir}/coverage ${artifactdir}/coverage
 fi
