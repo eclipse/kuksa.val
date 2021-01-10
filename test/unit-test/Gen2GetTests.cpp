e164e3815fd4f2699c9c7171fab2cc288ffa2173
Sebastian Schildt <sebastian.schildt@de.bosch.com>
2021-01-10 16:13:01 +0100

    Repair Gen2Get Unit Test Harness and expectations
    Signed-off-by: Sebastian Schildt <sebastian.schildt@de.bosch.com>

--- test/unit-test/Gen2GetTests.cpp
+++ test/unit-test/Gen2GetTests.cpp
@@ -14,68 +14,84 @@
 
 /** This are tests for VIS Gen2-style get request. No access checks */
 
-
-
 #include <boost/test/unit_test.hpp>
 #include <turtle/mock.hpp>
 
 #include <memory>
 #include <string>
 
-#include "WsChannel.hpp"
-#include "ILoggerMock.hpp"
-#include "IVssDatabaseMock.hpp"
+#include "IAccessCheckerMock.hpp"
 #include "IAuthenticatorMock.hpp"
+#include "ILoggerMock.hpp"
 #include "ISubscriptionHandlerMock.hpp"
+#include "WsChannel.hpp"
+
+#include "exception.hpp"
 
 #include "VSSPath.hpp"
 
-#include "exception.hpp"
 #include "JsonResponses.hpp"
 #include "VssCommandProcessor.hpp"
+#include "VssDatabase.hpp"
+
+#include "exception.hpp"
 
 namespace {
-  // common resources for tests
-  std::shared_ptr<ILoggerMock> logMock;
-  std::shared_ptr<IVssDatabaseMock> dbMock;
-  std::shared_ptr<IAuthenticatorMock> authMock;
-  std::shared_ptr<ISubscriptionHandlerMock> subsHndlMock;
+// common resources for tests
+std::string validFilename{"test_vss_rel_2.0.json"};
 
-  std::unique_ptr<VssCommandProcessor> processor;
+std::shared_ptr<ILoggerMock> logMock;
+std::shared_ptr<IAuthenticatorMock> authMock;
+std::shared_ptr<IAccessCheckerMock> accCheckMock;
 
-  // Pre-test initialization and post-test desctruction of common resources
-  struct TestSuiteFixture {
-    TestSuiteFixture() {
-      logMock = std::make_shared<ILoggerMock>();
-      dbMock = std::make_shared<IVssDatabaseMock>();
-      authMock = std::make_shared<IAuthenticatorMock>();
-      subsHndlMock = std::make_shared<ISubscriptionHandlerMock>();
+std::shared_ptr<ISubscriptionHandlerMock> subHandlerMock;
 
-      processor = std::make_unique<VssCommandProcessor>(logMock, dbMock, authMock, subsHndlMock);
-    }
-    ~TestSuiteFixture() {
-      logMock.reset();
-      dbMock.reset();
-      authMock.reset();
-      subsHndlMock.reset();
-      processor.reset();
-    }
-  };
-}
+std::shared_ptr<VssDatabase> db;
 
-// Define name of test suite and define test suite fixture for pre and post test handling
+std::unique_ptr<VssCommandProcessor> processor;
+
+// Pre-test initialization and post-test desctruction of common resources
+struct TestSuiteFixture {
+  TestSuiteFixture() {
+    logMock = std::make_shared<ILoggerMock>();
+    authMock = std::make_shared<IAuthenticatorMock>();
+
+    accCheckMock = std::make_shared<IAccessCheckerMock>();
+    subHandlerMock = std::make_shared<ISubscriptionHandlerMock>();
+
+    std::string vss_file{"test_vss_rel_2.0.json"};
+
+
+    MOCK_EXPECT(logMock->Log).at_least(0);  // ignore log events
+    db = std::make_shared<VssDatabase>(logMock, subHandlerMock, accCheckMock);
+    db->initJsonTree(vss_file);
+
+    //    processor = std::make_unique<VssCommandProcessor>(logMock, dbMock,
+    //    authMock, subHandlerMock);
+    processor = std::make_unique<VssCommandProcessor>(logMock, db, authMock,
+                                                      subHandlerMock);
+
+  }
+  ~TestSuiteFixture() {
+    logMock.reset();
+    accCheckMock.reset();
+    authMock.reset();
+    subHandlerMock.reset();
+    processor.reset();
+  }
+};
+}  // namespace
+
+// Define name of test suite and define test suite fixture for pre and post test
+// handling
 BOOST_FIXTURE_TEST_SUITE(Gen2GetTests, TestSuiteFixture);
 
-//GetVallidpath
-//GetNonExistingPatjh
-//getBRanch
-//getWildcard End
-//getWildcardMiddle
-//getNonExistingWildcardMiddle
+// getBRanch
+// getWildcard End
+// getWildcardMiddle
+// getNonExistingWildcardMiddle
 
-
-BOOST_AUTO_TEST_CASE(Gen2_Get_Sensor)
-{
+BOOST_AUTO_TEST_CASE(Gen2_Get_Sensor) {
   WsChannel channel;
 
   jsoncons::json jsonGetRequestForSignal;
@@ -85,7 +101,6 @@ BOOST_AUTO_TEST_CASE(Gen2_Get_Sensor)
   std::string path{"Vehicle/Speed"};
   const VSSPath vss_path = VSSPath::fromVSS(path);
 
-  
   // setup
   channel.setAuthorized(false);
   channel.setConnID(1);
@@ -97,41 +112,33 @@ BOOST_AUTO_TEST_CASE(Gen2_Get_Sensor)
   std::string expectedJsonString{R"(
       {
     "action": "get", 
-    "path": "Vehicle.Speed", 
+    "path": "Vehicle/Speed", 
     "requestId": "1", 
     "value": "---"
     }
       )"};
   jsoncons::json expectedJson = jsoncons::json::parse(expectedJsonString);
 
+  //Read access has been checked
+  MOCK_EXPECT(accCheckMock->checkReadNew).once().with(mock::any,vss_path).returns(true);
   
-  // expectations
-  // validate that at least one log event was processed
-  MOCK_EXPECT(logMock->Log).at_least( 1 );
-
-  
-    MOCK_EXPECT(dbMock->getSignal2)
-    .once()
-    .with(mock::any, mock::equal(vss_path) ,false)
-    .returns(expectedJson);
-  
-
   // run UUT
-  auto resStr = processor->processQuery(jsonGetRequestForSignal.as_string(), channel);
+  auto resStr =
+      processor->processQuery(jsonGetRequestForSignal.as_string(), channel);
   auto res = json::parse(resStr);
 
-  //Does result have a timestamp?
+  // Does result have a timestamp?
   BOOST_TEST(res["timestamp"].as<int>() > 0);
-  
-  //Remove timestamp for comparision purposes
-  expectedJson["timestamp"]=res["timestamp"].as<int>();
-  
+
+  // Remove timestamp for comparision purposes
+  expectedJson["timestamp"] = res["timestamp"].as<int>();
+
   BOOST_TEST(res == expectedJson);
 }
 
 
-BOOST_AUTO_TEST_CASE(Gen2_Get_NonExistingPath)
-{
+
+BOOST_AUTO_TEST_CASE(Gen2_Get_NonExistingPath) {
   WsChannel channel;
 
   jsoncons::json jsonGetRequestForSignal;
@@ -141,7 +148,6 @@ BOOST_AUTO_TEST_CASE(Gen2_Get_NonExistingPath)
   std::string path{"Vehicle/OBD/FluxCapacitorCharge"};
   const VSSPath vss_path = VSSPath::fromVSS(path);
 
-  
   // setup
   channel.setAuthorized(false);
   channel.setConnID(1);
@@ -152,23 +158,115 @@ BOOST_AUTO_TEST_CASE(Gen2_Get_NonExistingPath)
 
   JsonResponses::pathNotFound(requestId, "get", path, jsonPathNotFound);
 
-  // expectations
-  // validate that at least one log event was processed
-  MOCK_EXPECT(logMock->Log).at_least( 1 );
-
-  //expect a call to getSignal2 with negative result
-  MOCK_EXPECT(dbMock->getSignal2)
-    .once()
-    .with(mock::any, mock::equal(vss_path) ,false)
-    .returns(jsonPathNotFound);
-
+  
   // run UUT
-  auto resStr = processor->processQuery(jsonGetRequestForSignal.as_string(), channel);
+  auto resStr =
+      processor->processQuery(jsonGetRequestForSignal.as_string(), channel);
   auto res = json::parse(resStr);
 
   // verify
-
   BOOST_TEST(res == jsonPathNotFound);
+}
+
+
+BOOST_AUTO_TEST_CASE(Gen2_Get_Branch) {
+  WsChannel channel;
+
+  jsoncons::json jsonGetRequestForSignal;
+  jsoncons::json jsonPathNotFound;
+
+  string requestId = "1";
+  std::string path{"Vehicle/VehicleIdentification"};
+  const VSSPath vss_path = VSSPath::fromVSS(path);
+
+  // setup
+  channel.setAuthorized(false);
+  channel.setConnID(1);
+
+  jsonGetRequestForSignal["action"] = "get";
+  jsonGetRequestForSignal["path"] = path;
+  jsonGetRequestForSignal["requestId"] = requestId;
+
+  std::string expectedJsonString{R"(
+     {
+    "action": "get", 
+    "requestId": "1", 
+    "value": [
+        {
+            "Vehicle/VehicleIdentification/vehicleinteriorType": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/vehicleinteriorColor": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/vehicleSpecialUsage": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/vehicleSeatingCapacity": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/vehicleModelDate": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/vehicleConfiguration": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/purchaseDate": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/productionDate": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/meetsEmissionStandard": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/knownVehicleDamages": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/dateVehicleFirstRegistered": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/bodyType": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/Year": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/WMI": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/VIN": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/Model": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/Brand": "---"
+        }, 
+        {
+            "Vehicle/VehicleIdentification/ACRISSCode": "---"
+        }
+    ]
+}
+      )"};
+  jsoncons::json expectedJson = jsoncons::json::parse(expectedJsonString);
+
+  MOCK_EXPECT(accCheckMock->checkReadNew).once().with(mock::any,vss_path).returns(true);
+  MOCK_EXPECT(accCheckMock->checkReadNew).with(mock::any,mock::any).returns(true);
+
+
+  // run UUT
+  auto resStr =
+      processor->processQuery(jsonGetRequestForSignal.as_string(), channel);
+  auto res = json::parse(resStr);
+
+  // Does result have a timestamp?
+  BOOST_TEST(res["timestamp"].as<int>() > 0);
+
+  // Remove timestamp for comparision purposes
+  expectedJson["timestamp"] = res["timestamp"].as<int>();
+
+  BOOST_TEST(res == expectedJson);
 }
 
 }
