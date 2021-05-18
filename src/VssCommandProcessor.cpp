084727293d6c8a456bc8b146cf2131263f35ed94
Sebastian Schildt <sebastian.schildt@de.bosch.com>
2021-05-18 19:57:20 +0200

    Fix updateVSSTree
    Signed-off-by: Sebastian Schildt <sebastian.schildt@de.bosch.com>

--- src/VssCommandProcessor.cpp
+++ src/VssCommandProcessor.cpp
@@ -448,7 +448,7 @@ string VssCommandProcessor::processQuery(const string &req_json,
     } else if (action == "updateVSSTree") {
       string request_id = root["requestId"].as<string>();
       logger->Log(LogLevel::VERBOSE, "VssCommandProcessor::processQuery: update MetaData query  for with request id " + request_id);
-      response = processUpdateVSSTree(channel, request_id, root["updateadata"]);
+      response = processUpdateVSSTree(channel, request_id, root["metadata"]);
     } else {
       string path = root["path"].as<string>();
       string request_id = root["requestId"].as<string>();
