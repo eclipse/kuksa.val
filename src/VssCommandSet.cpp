013e222c76792ef127c653ef641197a91cfc4070
Sebastian Schildt <sebastian.schildt@de.bosch.com>
2021-07-07 12:35:22 +0200

    Better verbose logging when a set failed
    Signed-off-by: Sebastian Schildt <sebastian.schildt@de.bosch.com>

--- src/VssCommandSet.cpp
+++ src/VssCommandSet.cpp
@@ -62,11 +62,15 @@ std::string VssCommandProcessor::processSet2(WsChannel &channel,
   //(set all or none)
   for ( std::tuple<VSSPath,jsoncons::json> setTuple : setPairs) {
     if (! database->pathExists(std::get<0>(setTuple) )) {
+      stringstream msg;
+      msg << "Path " << std::get<0>(setTuple).to_string() << " does not exist";
+      logger->Log(LogLevel::VERBOSE,msg.str());
       return JsonResponses::pathNotFound(request["requestId"].as<string>(), "set", std::get<0>(setTuple).to_string());
     }
     if (! accessValidator_->checkWriteAccess(channel, std::get<0>(setTuple) )) {
       stringstream msg;
       msg << "No write access to " << std::get<0>(setTuple).to_string();
+      logger->Log(LogLevel::VERBOSE,msg.str());
       return JsonResponses::noAccess(request["requestId"].as<string>(), "set", msg.str());
     }
     if (! database->pathIsWritable(std::get<0>(setTuple))) {
