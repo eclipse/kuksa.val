1ed2f4a9d24fffa187dd152a52e81267546fc4d4
Sebastian Schildt <sebastian.schildt@de.bosch.com>
2021-07-08 19:35:11 +0200

    Check for and print error messages from kuksa.val server
    Signed-off-by: Sebastian Schildt <sebastian.schildt@de.bosch.com>

--- kuksa_feeders/dbc2val/dbcfeeder.py
+++ kuksa_feeders/dbc2val/dbcfeeder.py
@@ -14,6 +14,7 @@
 import os, sys, signal
 import configparser
 import queue
+import json
 
 import dbc2vssmapper
 import dbcreader
@@ -89,7 +90,12 @@ while running:
             tv=mapping.transform(signal,target,value)
             if tv is not None: #none indicates the transform decided to not set the value
                 print("Update VSS path {} to {} based on signal {}".format(target, tv, signal))
-                kuksa.setValue(target, tv)
+                resp=json.loads(kuksa.setValue(target, tv))
+                if "error" in resp:
+                    if "message" in resp["error"]: 
+                        print("Error setting {}: {}".format(target, resp["error"]["message"]))
+                    else:
+                        print("Unknown error setting {}: {}".format(target, resp))
             pass
     except queue.Empty:
         pass
