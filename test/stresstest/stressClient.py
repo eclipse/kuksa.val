377f85c7f67dbf3753506c69c8dce265dc0fc637
linussieweke <et19013@lehre.dhbw-stuttgart.de>
2021-08-26 12:26:51 +0200

    Bug fixing for stresstest
    Signed-off-by: linussieweke <et19013@lehre.dhbw-stuttgart.de>

--- test/stresstest/stressClient.py
+++ test/stresstest/stressClient.py
@@ -29,26 +29,11 @@ class StressClient():
         self.commThread = KuksaClientThread(self.cfg)       #make new thread
         self.commThread.start()                             #start thread
 
-        pollIndex = 30
-        while(pollIndex > 0):
-            if self.commThread.wsConnected == True:
-                pollIndex = 0
-            else:
-                time.sleep(0.1)
-            pollIndex -= 1
-
-        if self.commThread.wsConnected:
-            print("Websocket connected!!")
-        else:
-            print("Websocket could not be connected!!")
-            self.commThread.stop()
-            self.commThread = None
-
     def __init__(self):
-        cfg = self.getConfig()
+        self.cfg = self.getConfig()
         try:
             self.connect(self.cfg)
-            self.commThread.timeout = cfg.getfloat("timeout", 0.1)                       #get necessary timeout for stresstest
+            self.commThread.timeout = self.cfg.getfloat("timeout", fallback=0.1)                       #get necessary timeout for stresstest
             self.commThread.authorize(token=self.commThread.tokenfile)
         except:
             print("Could not connect successfully")
