39dc7b214420584e68a7683d26a72bc12ad9e631
nayakned <naresh.nayak@de.bosch.com>
2022-04-22 12:04:07 +0200

    Minor fixes/improvements to the golang client.    -  Added .gitignore.    -  Changes from golang linter.    -  Fixed unreachable code.
    Signed-off-by: nayakned <naresh.nayak@de.bosch.com>

--- kuksa_go_client/kuksa_viss/api.go
+++ kuksa_go_client/kuksa_viss/api.go
@@ -1,126 +1,126 @@
 package kuksa_viss
 
 import (
-    "log"
-    "os"
+	"log"
+	"os"
 
-    "github.com/google/uuid"
-    "github.com/stretchr/objx"
+	"github.com/google/uuid"
+	"github.com/stretchr/objx"
 )
 
 // Function to connect to kuksa.val
 func (cc *KuksaClientComm) ConnectToKuksaValServerWs() error {
-    err := cc.startCommunication()
-    return err
+	err := cc.startCommunication()
+	return err
 }
 
 // Function to get value from VSS tree
 func (cc *KuksaClientComm) GetValueFromKuksaValServer(path string) (string, error) {
 
-    // Create new KuksaValRequest
-    req := objx.New(make (map [string] interface{}))
-    req.Set("requestId", uuid.New().String())
-    req.Set("action", "get")
-    req.Set("path",  path)
+	// Create new KuksaValRequest
+	req := objx.New(make(map[string]interface{}))
+	req.Set("requestId", uuid.New().String())
+	req.Set("action", "get")
+	req.Set("path", path)
 
-    resp, err := cc.communicationHandler(req)
-    var value string
-    if resp.Has("data.dp.value") {
-        value = resp.Get("data.dp.value").String()
-    }
-    return value, err
+	resp, err := cc.communicationHandler(req)
+	var value string
+	if resp.Has("data.dp.value") {
+		value = resp.Get("data.dp.value").String()
+	}
+	return value, err
 }
 
 // Function to subscribe value from VSS tree
 func (cc *KuksaClientComm) SubscribeFromKuksaValServer(path string, subsChannel *chan []byte) (string, error) {
 
-    // Create new KuksaValRequest
-    req := objx.New(make (map [string] interface{}))
-    req.Set("requestId", uuid.New().String())
-    req.Set("action", "subscribe")
-    req.Set("path",  path)
+	// Create new KuksaValRequest
+	req := objx.New(make(map[string]interface{}))
+	req.Set("requestId", uuid.New().String())
+	req.Set("action", "subscribe")
+	req.Set("path", path)
 
-    // Create channel for subscription
-    resp, err := cc.communicationHandler(req)
-    var subscriptionId string
-    if err == nil {
-        subscriptionId = resp.Get("subscriptionId").String()
-        cc.subscriptionStateMutex.Lock()
-        cc.subscriptionState[subscriptionId] = subsChannel
-        cc.subscriptionStateMutex.Unlock()
+	// Create channel for subscription
+	resp, err := cc.communicationHandler(req)
+	var subscriptionId string
+	if err == nil {
+		subscriptionId = resp.Get("subscriptionId").String()
+		cc.subscriptionStateMutex.Lock()
+		cc.subscriptionState[subscriptionId] = subsChannel
+		cc.subscriptionStateMutex.Unlock()
 
-    }
-    return subscriptionId, err
+	}
+	return subscriptionId, err
 }
 
 // Function to unsubscribe value from VSS tree
 func (cc *KuksaClientComm) UnsubscribeFromKuksaValServer(id string) error {
 
-    // Create new KuksaValRequest
-    req := objx.New(make (map [string] interface{}))
-    req.Set("requestId", uuid.New().String())
-    req.Set("action", "unsubscribe")
-    req.Set("subscriptionId",  id)
+	// Create new KuksaValRequest
+	req := objx.New(make(map[string]interface{}))
+	req.Set("requestId", uuid.New().String())
+	req.Set("action", "unsubscribe")
+	req.Set("subscriptionId", id)
 
-    _, err := cc.communicationHandler(req)
-    if err == nil {
-        cc.subscriptionStateMutex.Lock()
-        delete(cc.subscriptionState, id)
-        cc.subscriptionStateMutex.Unlock()
-    }
-    return err
+	_, err := cc.communicationHandler(req)
+	if err == nil {
+		cc.subscriptionStateMutex.Lock()
+		delete(cc.subscriptionState, id)
+		cc.subscriptionStateMutex.Unlock()
+	}
+	return err
 }
 
 // Function to set value from VSS tree
 func (cc *KuksaClientComm) SetValueFromKuksaValServer(path string, value string) error {
 
-    // Create new KuksaValRequest
-    respChannel := make(chan objx.Map)
-    defer close(respChannel)
-    req := objx.New(make (map [string] interface{}))
-    uuid := uuid.New().String()
-    req.Set("requestId", uuid)
-    req.Set("action", "set")
-    req.Set("path",  path)
-    req.Set("value",  value)
+	// Create new KuksaValRequest
+	respChannel := make(chan objx.Map)
+	defer close(respChannel)
+	req := objx.New(make(map[string]interface{}))
+	uuid := uuid.New().String()
+	req.Set("requestId", uuid)
+	req.Set("action", "set")
+	req.Set("path", path)
+	req.Set("value", value)
 
-    _, err := cc.communicationHandler(req)
-    return err
+	_, err := cc.communicationHandler(req)
+	return err
 }
 
 // Function to authorize to kuksa.val server
 func (cc *KuksaClientComm) AuthorizeKuksaValServerConn() error {
 
-    // Get the authorization token
-    tokenByteString, err := os.ReadFile(cc.Config.TokenPath)
-    if err != nil {
-        log.Fatal("Error reading token: ",  err)
-        return err
-    }
+	// Get the authorization token
+	tokenByteString, err := os.ReadFile(cc.Config.TokenPath)
+	if err != nil {
+		log.Fatal("Error reading token: ", err)
+		return err
+	}
 
-    // Create new KuksaValRequest
-    req := objx.New(make (map [string] interface{}))
-    req.Set("requestId", uuid.New().String())
-    req.Set("action", "authorize")
-    req.Set("tokens",  string(tokenByteString))
+	// Create new KuksaValRequest
+	req := objx.New(make(map[string]interface{}))
+	req.Set("requestId", uuid.New().String())
+	req.Set("action", "authorize")
+	req.Set("tokens", string(tokenByteString))
 
-    _, err = cc.communicationHandler(req)
-    return err
+	_, err = cc.communicationHandler(req)
+	return err
 }
 
 // Function to get metadata from kuksa.val server
 func (cc *KuksaClientComm) GetMetadataFromKuksaValServer(path string) (string, error) {
 
-    // Create new KuksaValRequest
-    req := objx.New(make (map [string] interface{}))
-    req.Set("requestId", uuid.New().String())
-    req.Set("action", "getMetaData")
-    req.Set("path",  path)
+	// Create new KuksaValRequest
+	req := objx.New(make(map[string]interface{}))
+	req.Set("requestId", uuid.New().String())
+	req.Set("action", "getMetaData")
+	req.Set("path", path)
 
-    resp, err := cc.communicationHandler(req)
-    var value string
-    if resp.Has("metadata") {
-        value, err  = resp.Get("metadata").ObjxMap().JSON()
-    }
-    return value, err
+	resp, err := cc.communicationHandler(req)
+	var value string
+	if resp.Has("metadata") {
+		value, err = resp.Get("metadata").ObjxMap().JSON()
+	}
+	return value, err
 }
--- kuksa_go_client/kuksa_viss/config.go
+++ kuksa_go_client/kuksa_viss/config.go
@@ -1,63 +1,61 @@
 package kuksa_viss
 
 import (
-    "log"
-    "strconv"
-    
-    "github.com/spf13/viper"
+	"log"
+	"strconv"
+
+	"github.com/spf13/viper"
 )
 
 type Transport string
 
-
 const (
-    UndefTransport Transport = ""
-    Grpc                     = "GRPC"
-    WebSocket Transport      = "WebSocket"
+	UndefTransport Transport = ""
+	Grpc                     = "GRPC"
+	WebSocket      Transport = "WebSocket"
 )
 
 type KuksaVISSClientConfig struct {
-    ServerAddress           string  `mapstructure:"serverAddress"`
-    ServerPort              string  `mapstructure:"serverPort"`
-    Insecure                bool    `mapstructure:"insecure"`
-    CertsDir                string  `mapstructure:"certsDir"`
-    TokenPath               string  `mapstructure:"tokenPath"`
-    TransportProtocol       Transport    `mapstructure:"transport"`    
+	ServerAddress     string    `mapstructure:"serverAddress"`
+	ServerPort        string    `mapstructure:"serverPort"`
+	Insecure          bool      `mapstructure:"insecure"`
+	CertsDir          string    `mapstructure:"certsDir"`
+	TokenPath         string    `mapstructure:"tokenPath"`
+	TransportProtocol Transport `mapstructure:"transport"`
 }
 
 func ReadConfig(config *KuksaVISSClientConfig) {
 
-    // Read in the configuration of the switcher
-    log.Println("Reading Config ...")
+	// Read in the configuration of the switcher
+	log.Println("Reading Config ...")
 
-    viper.SetConfigName("kuksa-viss-client")   // name of config file (without extension)
-    viper.AddConfigPath("./")       // path to look for the config file in
+	viper.SetConfigName("kuksa-viss-client") // name of config file (without extension)
+	viper.AddConfigPath("./")                // path to look for the config file in
 
-    viper.SetEnvPrefix("kuksa_viss_client")
-    viper.AutomaticEnv()
-    err := viper.ReadInConfig()     // Find and read the config file
-    if err != nil {                 // Handle errors reading the config file
-	    log.Panicf("Fatal error config file: %s \n", err)
-    }
+	viper.SetEnvPrefix("kuksa_viss_client")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		log.Panicf("Fatal error config file: %s \n", err)
+	}
 
-    err = viper.Unmarshal(&config)
-    if err != nil {
-        log.Panicf("Unable to decode config into struct, %v", err)
-    }
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		log.Panicf("Unable to decode config into struct, %v", err)
+	}
 }
-
 
 func (config KuksaVISSClientConfig) String() string {
 
-    var retString string
+	var retString string
 
-    retString += "\nKuksa VISS Client Config\n"
-    retString += "\tServer Address: " + config.ServerAddress + "\n"
-    retString += "\tServer Port: " + config.ServerPort + "\n"
-    retString += "\tInsecure: " + strconv.FormatBool(config.Insecure) + "\n"
-    retString += "\tCertsDir: " + config.CertsDir + "\n"
-    retString += "\tTokenPath: " + config.TokenPath + "\n"
-    retString += "\tTransport Protocol: " + string(config.TransportProtocol) + "\n"
+	retString += "\nKuksa VISS Client Config\n"
+	retString += "\tServer Address: " + config.ServerAddress + "\n"
+	retString += "\tServer Port: " + config.ServerPort + "\n"
+	retString += "\tInsecure: " + strconv.FormatBool(config.Insecure) + "\n"
+	retString += "\tCertsDir: " + config.CertsDir + "\n"
+	retString += "\tTokenPath: " + config.TokenPath + "\n"
+	retString += "\tTransport Protocol: " + string(config.TransportProtocol) + "\n"
 
-    return retString
+	return retString
 }
