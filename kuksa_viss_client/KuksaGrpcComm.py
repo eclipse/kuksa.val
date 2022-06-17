38ae1edc3864dfc17cd4e4e911735deee1270bf3
nayakned <naresh.nayak@de.bosch.com>
2022-06-17 11:18:31 +0200

    Implemented gRPC calls to get, set and authorize in kuksa VISS Client.
    Signed-off-by: nayakned <naresh.nayak@de.bosch.com>

--- kuksa_viss_client/KuksaGrpcComm.py
+++ kuksa_viss_client/KuksaGrpcComm.py
@@ -10,8 +10,10 @@
 # SPDX-License-Identifier: EPL-2.0
 ########################################################################
 
+from http import client
 import queue, os
-import grpc, asyncio
+import grpc, asyncio, json
+from google.protobuf.json_format import MessageToJson
 
 import kuksa_pb2
 import kuksa_pb2_grpc
@@ -38,6 +40,14 @@ class KuksaGrpcComm:
         self.recvMsgQueues = {}
         self.run = False
 
+        self.AttrDict = {
+            "value": kuksa_pb2.RequestType.CURRENT_VALUE,
+            "targetValue": kuksa_pb2.RequestType.TARGET_VALUE,
+            "metadata": kuksa_pb2.RequestType.METADATA
+        }
+
+        self.grpcConnectionAuthToken = None
+
     # Function to check connection status
     def checkConnection(self):
         if self.grpcConnected:
@@ -50,29 +60,94 @@ class KuksaGrpcComm:
         self.run = False
         print("gRPC channel disconnected!!")
 
-    # Receive handler
-    async def _receiver_handler(self, clientStub):
+    # Function to implement get
+    def getValue(self, path, attribute="value", timeout = 5):
+        recvQueue = queue.Queue(maxsize=1)
 
-        while self.run:
-            await asyncio.sleep(0.1)
+        # Create Get Request
+        req = kuksa_pb2.GetRequest()
+        if attribute in self.AttrDict.keys():
+            pass
+        else:
+            attribute = "value"
+        req.type = self.AttrDict[attribute]
+        req.path.append(path)
+        self.sendMsgQueue.put(("get", req, recvQueue))
+        resp = recvQueue.get(timeout=timeout)
+        respJson = MessageToJson(resp)
+        return respJson        
 
-    # Send handler
-    async def _sender_handler(self, clientStub):
+    # Function to implement set
+    def setValue(self, path, value, attribute="value", timeout = 5):
+        recvQueue = queue.Queue(maxsize=1)
 
-        while self.run:
-            await asyncio.sleep(0.1)
+        # Create Set Request
+        req = kuksa_pb2.SetRequest()
+        if attribute in self.AttrDict.keys():
+            pass
+        else:
+            attribute = "value"
+        req.type = self.AttrDict[attribute]
+        val = kuksa_pb2.Value()
+        val.path = path
+        val.valueString = value
+        req.values.append(val)
+
+        self.sendMsgQueue.put(("set", req, recvQueue))
+        resp = recvQueue.get(timeout=timeout)
+        respJson = MessageToJson(resp)
+        return respJson
+
+    # Function for authorization
+    def authorize(self, token=None, timeout=2):
+        recvQueue = queue.Queue(maxsize=1)
+        if token == None:
+            token = self.tokenfile
+        token = os.path.expanduser(token)
+        if os.path.isfile(token):
+            with open(token, "r") as f:
+                token = f.readline()
+
+        # Create AuthRequest
+        req = kuksa_pb2.AuthRequest()
+        req.token = token
+        self.sendMsgQueue.put(("authorize", req, recvQueue))
+        resp = recvQueue.get(timeout=timeout)
+        respJson = MessageToJson(resp)
+        self.grpcConnectionAuthToken = resp.connectionId
+        return respJson
+
 
     # Async function to handle the gRPC calls
     async def _grpcHandler(self, clientStub):
+        
         # gRPC Socket is connected
         self.grpcConnected = True
         self.run = True
-        recv = asyncio.Task(self._receiver_handler(clientStub))
-        send = asyncio.Task(self._sender_handler(clientStub))
+        while self.run:
+            try:
+                (call, requestObj, responseQueue) = self.sendMsgQueue.get_nowait()
+            except queue.Empty:
+                await asyncio.sleep(0.01)
+                continue
+            except Exception as e:
+                print("gRPCHandler Exception: " + str(e))
+                continue
 
-        await asyncio.wait([recv, send], return_when=asyncio.FIRST_COMPLETED)
-        recv.cancel()
-        send.cancel()
+            try:
+                md = (("connectionid", self.grpcConnectionAuthToken),)
+                if call == "get":
+                    respObj = await clientStub.get(requestObj, metadata=md)
+                elif call == "set":
+                    respObj = await clientStub.set(requestObj, metadata=md)
+                elif call == "authorize":
+                    respObj = await clientStub.authorize(requestObj, metadata=md)        
+                else:
+                    raise Exception("Not Implemented!!")
+                responseQueue.put(respObj)
+            except Exception as e:
+                print("gRPCHandler Exception: " + str(e))
+                
         self.grpcConnected = False
 
     # Main loop for handling gRPC communication
@@ -100,7 +175,7 @@ class KuksaGrpcComm:
             creds = grpc.ssl_channel_credentials(root_certificates=caCert, private_key=clientKey, certificate_chain=clientCert)
             async with grpc.aio.secure_channel(serverAddr, creds) as channel:
                 print("Secure gRPC channel created!!")
-                # USe the gRPC Stubs
+                # Use the gRPC Stubs
                 clientStub = kuksa_pb2_grpc.kuksa_grpc_ifStub(channel)
                 await self._grpcHandler(clientStub)
 
