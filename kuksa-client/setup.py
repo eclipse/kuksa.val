eb5997715326abff74e68f1282d7909c85d9ae69
Romain Létendart (BEG/ENG1-FR) <romain.letendart@fr.bosch.com>
2022-09-27 14:21:23 +0200

    setup: Move setuptools configurations to config files
    - setup_requires is deprecated thus moving its content into pyproject.toml's "[build-system] requires".
    - Only keep setuptools-git-versioning's options where values are different from default ones.

--- kuksa-client/setup.py
+++ kuksa-client/setup.py
@@ -24,17 +24,6 @@ class SDistCommand(BuildPackageProtos, sdist.sdist):
 
 
 setuptools.setup(
-    setuptools_git_versioning={
-        "template": "{tag}",
-        "dev_template": "{tag}-{ccount}",
-        "dirty_template": "{tag}-{ccount}",
-        "starting_version": "0.1.6",
-        "version_callback": None,
-        "version_file": None,
-        "count_commits_from_version_file": False
-    },
-    package_data={ "kuksa_client": ["logo"], "kuksa_certificates": ["*", "jwt/*"]},
-    setup_requires=['setuptools-git-versioning', 'grpcio', 'grpcio-tools'],
     cmdclass={
         'build': BuildCommand,
         'build_py': BuildPyCommand,  # Used for editable installs but also for building wheels
