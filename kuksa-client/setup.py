26da66c66ef9ca03a8e54fc6343e4412946cf104
Romain Létendart (BEG/ENG1-FR) <romain.letendart@fr.bosch.com>
2022-09-29 10:30:13 +0200

    setup: Stop patching generated pb2 modules
    Relative imports have proven flawed with protoc code generation so let
    us rely on absolute ones and stop patching to enforce relative ones.

--- kuksa-client/setup.py
+++ kuksa-client/setup.py
@@ -1,21 +1,12 @@
-import setuptools
 import distutils.command.build
-from glob import glob
-import os
+import setuptools
+
 
 class BuildCommand(distutils.command.build.build):
-
     def run(self):
         from grpc_tools import command
-        command.build_package_protos("./")
-        # protobuf for python is just hopelessly broken wrt. imports
-        # Read the whole story (and more elgant(?)/heavy-weight) solutions here
-        # https://github.com/protocolbuffers/protobuf/issues/1491
-
-        # we will just patch directly
-        os.system('sed -i "s/import kuksa_pb2 as kuksa__pb2/from . import kuksa_pb2 as kuksa__pb2/g" kuksa/val/kuksa_pb2_grpc.py')
-        # Run the original build command
-        distutils.command.build.build.run(self)
+        command.build_package_protos('.')
+        super().run()
 
 
 setuptools.setup(
