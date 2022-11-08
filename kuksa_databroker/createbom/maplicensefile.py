886454238d6d1c4e292b3f216ebc6757cb01a07b
Sebastian Schildt <sebastian.schildt@de.bosch.com>
2022-11-08 22:00:48 +0100

    Collecting BOM and licenses for RUST projects
    Signed-off-by: Sebastian Schildt <sebastian.schildt@de.bosch.com>

---
+++ kuksa_databroker/createbom/maplicensefile.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+########################################################################
+# Copyright (c) 2022 Robert Bosch GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+########################################################################
+
+
+MAP = {
+    "Apache-2.0": "Apache2.txt.gz",
+    "MIT": "MIT.txt.gz",
+    "Unlicense": "Unlicense.txt.gz",
+    "BSL-1.0": "BSL-1.0.txt.gz",
+    "Unicode-DFS-2016": "Unicode-DFS-2016.txt.gz"
+}
