8dee98a7fb40d681955121f4fc1e1ecada2879eb
Romain Létendart <romain.letendart@fr.bosch.com>
2022-11-25 17:07:35 +0100

    kuksa_client: Add a documented way to regenerate pb2 files

--- kuksa-client/setup.py
+++ kuksa-client/setup.py
@@ -9,9 +9,22 @@ from setuptools.command import sdist
 
 class BuildPackageProtos:
     def run(self):
+        self.run_command('build_pb2')
+        return super().run()
+
+
+class BuildPackageProtosCommand(setuptools.Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
         from grpc_tools import command  # pylint: disable=import-outside-toplevel
-        command.build_package_protos('.')
-        super().run()
+        command.build_package_protos('.', strict_mode=True)
 
 
 class BuildCommand(BuildPackageProtos, build.build):
@@ -29,6 +42,7 @@ class SDistCommand(BuildPackageProtos, sdist.sdist):
 setuptools.setup(
     cmdclass={
         'build': BuildCommand,
+        'build_pb2': BuildPackageProtosCommand,
         'build_py': BuildPyCommand,  # Used for editable installs but also for building wheels
         'sdist': SDistCommand,
     }
