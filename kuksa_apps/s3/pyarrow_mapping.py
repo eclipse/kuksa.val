4cf2ebe121e3a7fbec09c83c4dc311c44322daf6
Romain Létendart <romain.letendart@fr.bosch.com>
2022-12-07 08:17:03 +0100

    kuksa_apps.s3: Add databroker support
    This commit fixes issue #392.

--- kuksa_apps/s3/pyarrow_mapping.py
+++ kuksa_apps/s3/pyarrow_mapping.py
@@ -18,6 +18,8 @@
 
 import pyarrow as pa
 
+from kuksa_client.grpc import DataType
+
 KUKSA_TO_PYARROW_MAPPING = {
     "int8": pa.int8,
     "int16": pa.int16,
@@ -31,4 +33,31 @@ KUKSA_TO_PYARROW_MAPPING = {
     "double": pa.float64,
     "float": pa.float32,
     "string": pa.string,
+}
+
+KUKSA_CLIENT_TO_PYARROW_MAPPING = {
+    DataType.STRING: pa.string,
+    DataType.BOOLEAN: pa.bool_,
+    DataType.INT8: pa.int8,
+    DataType.INT16: pa.int16,
+    DataType.INT32: pa.int32,
+    DataType.INT64: pa.int64,
+    DataType.UINT8: pa.uint8,
+    DataType.UINT16: pa.uint16,
+    DataType.UINT32: pa.uint32,
+    DataType.UINT64: pa.uint64,
+    DataType.FLOAT: pa.float32,
+    DataType.DOUBLE: pa.float64,
+    DataType.STRING_ARRAY: pa.StringArray,
+    DataType.BOOLEAN_ARRAY: pa.BooleanArray,
+    DataType.INT8_ARRAY: pa.Int8Array,
+    DataType.INT16_ARRAY: pa.Int16Array,
+    DataType.INT32_ARRAY: pa.Int32Array,
+    DataType.INT64_ARRAY: pa.Int64Array,
+    DataType.UINT8_ARRAY: pa.UInt8Array,
+    DataType.UINT16_ARRAY: pa.UInt16Array,
+    DataType.UINT32_ARRAY: pa.UInt32Array,
+    DataType.UINT64_ARRAY: pa.UInt64Array,
+    DataType.FLOAT_ARRAY: pa.FloatingPointArray,
+    DataType.DOUBLE_ARRAY: pa.FloatingPointArray,
 }
