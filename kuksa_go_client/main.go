b4b1a3e253f798da19634b0a2c1f6c692bbeeaae
luksamittag <lukas.mittag@de.bosch.com>
2022-12-13 10:03:15 -0500

    Give the ability to auto generate the pb.go files and therefore install protobuf compiler

--- kuksa_go_client/main.go
+++ kuksa_go_client/main.go
@@ -12,132 +12,143 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:generate protoc --go_out=proto -I $HOME/kuksa.val/proto kuksa/val/v1/types.proto kuksa/val/v1/val.proto --go_opt=module=github.com/eclipse/kuksa.val/proto/kuksa/val/v1
+
 package main
 
 import (
 	"log"
 	"strconv"
 	"time"
+	"flag"
 
 	"github.com/eclipse/kuksa.val/kuksa_go_client/kuksa_viss"
+	"github.com/eclipse/kuksa.val/kuksa_go_client/protocInstall"
 )
 
 func main() {
-	log.Println("Starting Kuksa VISS Client!!")
+	protoc := flag.Bool("protoc", false, "")
+    flag.Parse()
 
-	// Load the Kuksa Configuration
-	var configKuksaClient kuksa_viss.KuksaVISSClientConfig
-	kuksa_viss.ReadConfig(&configKuksaClient)
-	log.Println(configKuksaClient)
+	if *protoc {
+		protocInstall.Protoc()
+	}else{
+		log.Println("Starting Kuksa VISS Client!!")
 
-	// Trigger connection to the Kuksa Server
-	kuksaClientComm := kuksa_viss.KuksaClientComm{Config: &configKuksaClient}
-	err := kuksaClientComm.ConnectToKuksaValServerWs()
-	if err != nil {
-		log.Fatalf("Connection Error: %v", err)
-	}
-	defer kuksaClientComm.Close()
+		// Load the Kuksa Configuration
+		var configKuksaClient kuksa_viss.KuksaVISSClientConfig
+		kuksa_viss.ReadConfig(&configKuksaClient)
+		log.Println(configKuksaClient)
 
-	// Authorize the connection
-	err = kuksaClientComm.AuthorizeKuksaValServerConn()
-	if err != nil {
-		log.Fatalf("Authorization Error: %v", err)
-	}
-
-	// Set and Get the attribute targetValue
-	err = kuksaClientComm.SetAttrValueFromKuksaValServer("Vehicle.Body.Trunk.IsOpen", "true", "targetValue")
-	if err != nil {
-		log.Printf("Set Attribute Error: %v", err)
-	} else {
-		log.Printf("Vehicle.Body.Trunk.IsOpen Set: True")
-	}
-
-	var value string
-	value, err = kuksaClientComm.GetAttrValueFromKuksaValServer("Vehicle.Body.Trunk.IsOpen", "targetValue")
-	if err != nil {
-		log.Printf("Get Attribute Error: %v", err)
-	} else {
-		log.Printf("Vehicle.Body.Trunk.IsOpen: " + value)
-	}
-
-	// Get Value of Vehicle.Speed1
-	// This datapoint does not exist in the VSS and should result in an error
-	value, err = kuksaClientComm.GetValueFromKuksaValServer("Vehicle.Speed1")
-	if err != nil {
-		log.Printf("Get Error: %v", err)
-	} else {
-		log.Printf("Vehicle.Speed: " + value)
-	}
-
-	// Go Routine for setting Vehicle Speed
-	go func() {
-		for i := 0; ; i++ {
-			// Set Value of Vehicle.Speed
-			err := kuksaClientComm.SetValueFromKuksaValServer("Vehicle.Speed", strconv.Itoa(i))
-			if err != nil {
-				log.Printf("Set Error: %v", err)
-			} else {
-				log.Printf("Vehicle.Speed Set: %d", i)
-			}
-			time.Sleep(10 * time.Millisecond)
+		// Trigger connection to the Kuksa Server
+		kuksaClientComm := kuksa_viss.KuksaClientComm{Config: &configKuksaClient}
+		err := kuksaClientComm.ConnectToKuksaValServerWs()
+		if err != nil {
+			log.Fatalf("Connection Error: %v", err)
 		}
-	}()
+		defer kuksaClientComm.Close()
 
-	// Go Routine for getting Vehicle Speed
-	finish := make(chan int)
-	go func() {
-		tick := time.Tick(15 * time.Millisecond)
-		done := time.After(1 * time.Second)
-		for {
-			select {
-			case <-tick:
-				// Get Value of Vehicle.Speed
-				value, err = kuksaClientComm.GetValueFromKuksaValServer("Vehicle.Speed")
+		// Authorize the connection
+		err = kuksaClientComm.AuthorizeKuksaValServerConn()
+		if err != nil {
+			log.Fatalf("Authorization Error: %v", err)
+		}
+
+		// Set and Get the attribute targetValue
+		err = kuksaClientComm.SetAttrValueFromKuksaValServer("Vehicle.Body.Trunk.IsOpen", "true", "targetValue")
+		if err != nil {
+			log.Printf("Set Attribute Error: %v", err)
+		} else {
+			log.Printf("Vehicle.Body.Trunk.IsOpen Set: True")
+		}
+
+		var value string
+		value, err = kuksaClientComm.GetAttrValueFromKuksaValServer("Vehicle.Body.Trunk.IsOpen", "targetValue")
+		if err != nil {
+			log.Printf("Get Attribute Error: %v", err)
+		} else {
+			log.Printf("Vehicle.Body.Trunk.IsOpen: " + value)
+		}
+
+		// Get Value of Vehicle.Speed1
+		// This datapoint does not exist in the VSS and should result in an error
+		value, err = kuksaClientComm.GetValueFromKuksaValServer("Vehicle.Speed1")
+		if err != nil {
+			log.Printf("Get Error: %v", err)
+		} else {
+			log.Printf("Vehicle.Speed: " + value)
+		}
+
+		// Go Routine for setting Vehicle Speed
+		go func() {
+			for i := 0; ; i++ {
+				// Set Value of Vehicle.Speed
+				err := kuksaClientComm.SetValueFromKuksaValServer("Vehicle.Speed", strconv.Itoa(i))
 				if err != nil {
-					log.Printf("Get Error: %v", err)
+					log.Printf("Set Error: %v", err)
 				} else {
-					log.Printf("Vehicle.Speed Get: %s", value)
+					log.Printf("Vehicle.Speed Set: %d", i)
 				}
-				continue
-			case <-done:
-				log.Printf("Done now!!")
-				finish <- 1
-				return
+				time.Sleep(10 * time.Millisecond)
 			}
+		}()
+
+		// Go Routine for getting Vehicle Speed
+		finish := make(chan int)
+		go func() {
+			tick := time.Tick(15 * time.Millisecond)
+			done := time.After(1 * time.Second)
+			for {
+				select {
+				case <-tick:
+					// Get Value of Vehicle.Speed
+					value, err = kuksaClientComm.GetValueFromKuksaValServer("Vehicle.Speed")
+					if err != nil {
+						log.Printf("Get Error: %v", err)
+					} else {
+						log.Printf("Vehicle.Speed Get: %s", value)
+					}
+					continue
+				case <-done:
+					log.Printf("Done now!!")
+					finish <- 1
+					return
+				}
+			}
+		}()
+
+		// Get MetaData of Vehicle.Speed
+		value, err = kuksaClientComm.GetMetadataFromKuksaValServer("Vehicle.Speed")
+		log.Printf("Vehicle.Speed Metadata: " + value)
+
+		// Subscribe to Vehicle.Speed
+		channel := make(chan []byte, 10)
+		id, err := kuksaClientComm.SubscribeFromKuksaValServer("Vehicle.Speed", &channel)
+		if err == nil {
+			log.Printf("Vehicle.Speed Subscription Id: %s", id)
+		} else {
+			log.Printf("Subscription Error %s", err)
 		}
-	}()
 
-	// Get MetaData of Vehicle.Speed
-	value, err = kuksaClientComm.GetMetadataFromKuksaValServer("Vehicle.Speed")
-	log.Printf("Vehicle.Speed Metadata: " + value)
+		go func() {
+			unsub := time.After(800 * time.Millisecond)
+			for {
+				select {
+				case val := <-channel:
+					log.Printf("Vehicle.Speed Subscribed: %s", val)
+				case <-unsub:
+					err := kuksaClientComm.UnsubscribeFromKuksaValServer(id)
+					if err == nil {
+						log.Printf("Vehicle.Speed Unsubscribed")
+					} else {
+						log.Printf("Unsubscription Error: %s", err)
+					}
+					close(channel)
+					return
+				}
+			}
+		}()
 
-	// Subscribe to Vehicle.Speed
-	channel := make(chan []byte, 10)
-	id, err := kuksaClientComm.SubscribeFromKuksaValServer("Vehicle.Speed", &channel)
-	if err == nil {
-		log.Printf("Vehicle.Speed Subscription Id: %s", id)
-	} else {
-		log.Printf("Subscription Error %s", err)
+		<-finish
 	}
-
-	go func() {
-		unsub := time.After(800 * time.Millisecond)
-		for {
-			select {
-			case val := <-channel:
-				log.Printf("Vehicle.Speed Subscribed: %s", val)
-			case <-unsub:
-				err := kuksaClientComm.UnsubscribeFromKuksaValServer(id)
-				if err == nil {
-					log.Printf("Vehicle.Speed Unsubscribed")
-				} else {
-					log.Printf("Unsubscription Error: %s", err)
-				}
-				close(channel)
-				return
-			}
-		}
-	}()
-
-	<-finish
 }
