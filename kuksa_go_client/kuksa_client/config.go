8de434e1a661dfb520fbe6cf709a2428365984c2
luksamittag <lukas.mittag@de.bosch.com>
2023-01-18 08:08:27 -0500

    Add and update license headers

--- kuksa_go_client/kuksa_client/config.go
+++ kuksa_go_client/kuksa_client/config.go
@@ -1,4 +1,4 @@
-// Copyright 2022 Robert Bosch GmbH
+// Copyright 2023 Robert Bosch GmbH
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
