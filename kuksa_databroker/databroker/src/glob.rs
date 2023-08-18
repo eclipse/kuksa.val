effe593517631f8ec92f1fd277eba77515f388e8
rafaeling <rafael.ruiz-lucena@de.bosch.com>
2023-08-18 14:48:28 +0200

    Generalize regex and fix unit tests

--- kuksa_databroker/databroker/src/glob.rs
+++ kuksa_databroker/databroker/src/glob.rs
@@ -61,39 +61,17 @@ pub fn to_regex_string_new(glob: &str) -> String {
     let mut re = String::from("^");
 
     // Replace all "standalone" wildcards with ".*"
-    if glob.ends_with("**") {
-        re.push_str(
-            &glob
-                .split('.')
-                .map(|part| match part {
-                    "*" => ".*",
-                    other => other,
-                })
-                .collect::<Vec<&str>>()
-                .join(r"\."),
-        );
-        // If it doesn't already end with a wildcard, add it
-        if !re.ends_with(".*") {
-            re.push_str(".*");
-        }
-    } else if glob.ends_with("*"){
-        re.push_str(
-            &glob
-                .split('.')
-                .map(|part| match part {
-                    "*" => "",
-                    other => other,
-                })
-                .collect::<Vec<&str>>()
-                .join(r"\."),
-        );
-        // If it doesn't already end with a wildcard, add it
-        if !re.ends_with("[^.]+") {
-            re.push_str("[^.]+");
-        }
-    }
-
-
+    re.push_str(
+        &glob
+            .split('.')
+            .map(|part| match part {
+                "*" => "[^.]+",
+                "**" => ".*",
+                other => other,
+            })
+            .collect::<Vec<&str>>()
+            .join(r"\."),
+    );
     // And finally, make sure we match until EOL
     re.push('$');
 
@@ -109,7 +87,7 @@ pub fn matches_path_pattern(input: &str) -> bool {
     let pattern = r"^(?:\w+(?:\.\w+)*)(?:\.\*+)?$";
     let regex = Regex::new(pattern).unwrap();
 
-    let pattern2 = r"^[^.]+(\.[^.]+)*\.(\*{2}|\*|[A-Za-z]+)(\.[^.]+)*\.[^.]+$";
+    let pattern2 = r"^(\*{2}|\*\.)?[^.]+(\.[^.]+)*\.(\*{2}|\*|[A-Za-z]+)(\.[^.]+)*\.[^.]+$";
     let regex2 = Regex::new(pattern2).unwrap();
 
     regex.is_match(input) || regex2.is_match(input)
@@ -117,47 +95,27 @@ pub fn matches_path_pattern(input: &str) -> bool {
 
 #[tokio::test]
 async fn test_valid_request_path() {
-    let inputs = vec![
-        "String.*",
-        "String.**",
-        "String.String.String.String.*",
-        "String.String.String.String.**",
-        "String.String.String.String",
-        "String.String.String.String.String.**",
-        "String.String.String.*.String",
-        "String.String.String.**.String",
-        "String.String.String.String.String.**.String",
-        "String.String.String.String.*.String.String",
-        "String.*.String.String",
-        "String.String.**.String.String",
-        "String.**.String.String",
-        "String.String.String.",
-        "String.String.String.String..",
-        "String.*.String.String..",
-        "*.String.String.String..",
-        "*.String.String.String",
-        "**.String.String.String.**",
-        "**.String.String.String.*",
-    ];
+    assert_eq!(matches_path_pattern("String.*"), true);
+    assert_eq!(matches_path_pattern("String.**"), true);
+    assert_eq!(matches_path_pattern("String.String.String.String.*"), true);
+    assert_eq!(matches_path_pattern("String.String.String.String.**"), true);
+    assert_eq!(matches_path_pattern("String.String.String.String"), true);
+    assert_eq!(matches_path_pattern("String.String.String.String.String.**"), true);
+    assert_eq!(matches_path_pattern("String.String.String.*.String"), true);
+    assert_eq!(matches_path_pattern("String.String.String.**.String"), true);
+    assert_eq!(matches_path_pattern("String.String.String.String.String.**.String"), true);
+    assert_eq!(matches_path_pattern("String.String.String.String.*.String.String"), true);
+    assert_eq!(matches_path_pattern("String.*.String.String"), true);
+    assert_eq!(matches_path_pattern("String.String.**.String.String"), true);
+    assert_eq!(matches_path_pattern("String.**.String.String"), true);
+    assert_eq!(matches_path_pattern("String.String.String."), false);
+    assert_eq!(matches_path_pattern("String.String.String.String.."), false);
+    assert_eq!(matches_path_pattern("String.*.String.String.."), false);
+    assert_eq!(matches_path_pattern("*.String.String.String.."), false);
+    assert_eq!(matches_path_pattern("*.String.String.String"), true);
+    assert_eq!(matches_path_pattern("**.String.String.String.**"), true);
+    assert_eq!(matches_path_pattern("**.String.String.String.*"), true);
+    assert_eq!(matches_path_pattern("**.String"), true);
+    //assert_eq!(matches_path_pattern("*.String"), true);
 
-    assert!(matches_path_pattern(inputs[0]));
-    assert!(matches_path_pattern(inputs[1]));
-    assert!(matches_path_pattern(inputs[2]));
-    assert!(matches_path_pattern(inputs[3]));
-    assert!(matches_path_pattern(inputs[4]));
-    assert!(matches_path_pattern(inputs[5]));
-    assert!(matches_path_pattern(inputs[6]));
-    assert!(matches_path_pattern(inputs[7]));
-    assert!(matches_path_pattern(inputs[8]));
-    assert!(matches_path_pattern(inputs[9]));
-    assert!(matches_path_pattern(inputs[10]));
-    assert!(matches_path_pattern(inputs[11]));
-    assert!(matches_path_pattern(inputs[12]));
-    assert!(!matches_path_pattern(inputs[13]));
-    assert!(!matches_path_pattern(inputs[14]));
-    assert!(!matches_path_pattern(inputs[15]));
-    assert!(!matches_path_pattern(inputs[16]));
-    assert!(matches_path_pattern(inputs[17]));
-    assert!(matches_path_pattern(inputs[18]));
-    assert!(matches_path_pattern(inputs[19]));
 }
