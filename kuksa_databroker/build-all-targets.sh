316e6cff0633e11fecb4d90fe4e63945659c4dd8
Sebastian Schildt <sebastian.schildt@de.bosch.com>
2023-11-08 22:22:56 +0100

    RISCV build databroker-cli -  Adressing minor review comments
    Signed-off-by: Sebastian Schildt <sebastian.schildt@de.bosch.com>

--- kuksa_databroker/build-all-targets.sh
+++ kuksa_databroker/build-all-targets.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
+# Copyright (c) 2023 Contributors to the Eclipse Foundation
+#
 # Building all currently supported targets.
 # Uses cross for cross-compiling. Needs to be executed
 # before docker build, as docker collects the artifacts
 # created by this script
 # this needs the have cross, cargo-license and createbom dependencies installed
+#
+# SPDX-License-Identifier: Apache-2.0
 
 # exit on error, to not waste any time
 set -e
