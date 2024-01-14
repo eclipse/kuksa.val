52761e6ce777488447d516720394295aa905980b
Akhil T Thomas <akhilthomasmec@gmail.com>
2024-01-14 16:10:50 +0100

    fix clippy warning for get_first
    Signed-off-by: Akhil T Thomas <akhilthomasmec@gmail.com>

--- kuksa_databroker/databroker/src/viss/v2/server.rs
+++ kuksa_databroker/databroker/src/viss/v2/server.rs
@@ -190,7 +190,7 @@ impl Viss for Server {
                         ts: SystemTime::now().into(),
                     }),
                     Err(errors) => {
-                        let error = if let Some((_, error)) = errors.get(0) {
+                        let error = if let Some((_, error)) = errors.first() {
                             match error {
                                 UpdateError::NotFound => Error::NotFoundInvalidPath,
                                 UpdateError::WrongType => Error::BadRequest {
